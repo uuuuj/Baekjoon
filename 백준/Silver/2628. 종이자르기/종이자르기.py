c016ce8ebe95457de4f6cfec859c9b31920bd621
uuuuj <105343092+uuuuj@users.noreply.github.com>
2023-08-16 15:01:20 +0900

    [Silver V] Title: 종이자르기, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/2628. 종이자르기/종이자르기.py
+++ 백준/Silver/2628. 종이자르기/종이자르기.py
@@ -1,33 +1,31 @@
 import sys
 
-W, H = map(int, sys.stdin.readline().split())
+A, B = map(int, sys.stdin.readline().split())
 
-T = int(sys.stdin.readline())
-
-x = [0, W]
-y = [0, H]
-
-for i in range(T):
-    direction, coord = map(int, sys.stdin.readline().split())
-    if direction == 0:
-        y.append(coord)
-
+N = int(sys.stdin.readline())
+width = [0, A]
+height = [0, B]
+for i in range(N):
+    x, y = map(int, sys.stdin.readline().split())
+    if x == 0:
+        height.append(y)
     else:
-        x.append(coord)
+        width.append(y)
 
-x.sort(reverse=True)
-y.sort(reverse=True)
+width.sort(reverse=True)
+height.sort(reverse=True)
+max_w = 0
+max_h = 0
+for i in range(len(width)-1):
+    if max_w < width[i] - width[i+1]:
+        max_w = width[i] - width[i+1]
+    else:
+        continue
 
-max_x = 0
-max_y = 0
-for i in range(len(x)-1):
-    if max_x < x[i] - x[i+1]:
-        max_x = x[i] - x[i+1]
+for i in range(len(height)-1):
+    if max_h < height[i] - height[i+1]:
+        max_h = height[i] - height[i+1]
+    else:
+        continue
 
-for i in range(len(y)-1):
-    if y[i] == y[-1]:
-        break
-    if max_y < y[i] - y[i+1]:
-        max_y = y[i] - y[i+1]
-
-print(max_x * max_y)
+print(max_h * max_w)
