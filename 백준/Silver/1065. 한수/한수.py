d7d1f55183a1f63a5cab00e1e41225016c875a22
uuuuj <105343092+uuuuj@users.noreply.github.com>
2023-08-16 14:36:33 +0900

    [Silver IV] Title: 한수, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1065. 한수/한수.py
+++ 백준/Silver/1065. 한수/한수.py
@@ -3,14 +3,13 @@ import sys
 N = int(sys.stdin.readline())
 cnt = 99
 if N < 100:
-    print(N)
-elif N == 100:
+    cnt = N
+    print(cnt)
+elif 100 == N:
     print(99)
 else:
     for i in range(101, N+1):
-        A = (i // 100) - (i%100)//10
-        B = ((i%100)//10) - ((i%100)%10)
-        if A == B:
+        if (i // 100) - (i % 100) // 10 == ( (i % 100) // 10 ) - ((i % 100) % 10):
             cnt += 1
         else:
             cnt += 0
