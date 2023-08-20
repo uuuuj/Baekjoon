c21f553acd7ba119deb832d0705e49833e780076
uuuuj <105343092+uuuuj@users.noreply.github.com>
2023-08-20 17:11:26 +0900

    [Gold V] Title: 괄호의 값, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Gold/2504. 괄호의 값/괄호의 값.py
@@ -0,0 +1,33 @@
+bracket = list(input())
+
+stack = []
+ans = 0
+tmp = 1
+
+for i in range(len(bracket)):
+    if bracket[i] == "(":
+        stack.append(bracket[i])
+        tmp *= 2
+    elif bracket[i] == "[":
+        stack.append(bracket[i])
+        tmp *= 3
+    elif bracket[i] == ")":
+        if not stack or stack[-1] == "[":
+            ans = 0
+            break
+        if bracket[i-1] == "(":
+            ans += tmp
+        stack.pop()
+        tmp //= 2
+    else:
+        if not stack or stack[-1] == "(":
+            ans = 0
+            break
+        if bracket[i-1] == "[":
+            ans += tmp
+        stack.pop()
+        tmp //= 3
+if stack:
+    print(0)
+else:
+    print(ans)
