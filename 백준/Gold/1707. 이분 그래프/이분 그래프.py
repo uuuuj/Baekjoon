de101decc962110919c650893b27d6a193068592
uuuuj <105343092+uuuuj@users.noreply.github.com>
2023-08-21 21:51:31 +0900

    [Gold IV] Title: 이분 그래프, Time: 1248 ms, Memory: 53920 KB -BaekjoonHub

--- 백준/Gold/1707. 이분 그래프/이분 그래프.py
+++ 백준/Gold/1707. 이분 그래프/이분 그래프.py
@@ -1,28 +1,27 @@
+#노드 정보들을 받고
 import sys
 sys.setrecursionlimit(10**6)
-k = int(input())
+K = int(sys.stdin.readline())
 
-
-def DFS(start, visited, graph, group):
-    visited[start] = group  # 현재 노드의 그룹 저장
-
-    # 인접 노드 탐색
-    for v in graph[start]:
-        if visited[v] == 0:  # 아직 방문하지 않은 노드
-            # -group : 현재 노드의 그룹과 다른 값 전달
-            result = DFS(v, visited, graph, -group)
-            if not result:
+def DFS(graph, v, visited, group):
+    visited[v] = group
+    for i in graph[v]:
+        #방문 안한 상태라면
+        if visited[i] == 0:
+            #현재 노드의 그룹과 다른 값 전달
+            result = DFS(graph, i, visited, -group)
+            if not result:#result가 False라면 즉 이분 그래프가 아니라면
                 return False
-        else:
-            if visited[v] == group:  # 이미 방문한 곳 중에서 노드가 현재 그룹과 같으면 이분 그래프가 아님
+        else:#연결된 노드와 그룹이 같다면
+            if visited[i] == group:
                 return False
     return True
 
 
-for _ in range(k):
+for _ in range(K):
     V, E = map(int, sys.stdin.readline().split())
     graph = [[] for _ in range(V+1)]
-    visited = [0] * (V+1)
+    visited = [0] * (V + 1)
     for _ in range(E):
         a, b = map(int, sys.stdin.readline().split())
         graph[a].append(b)
@@ -30,7 +29,9 @@ for _ in range(k):
 
     for i in range(1, V+1):
         if visited[i] == 0:
-            result = (DFS(i, visited, graph, 1))
+            result = (DFS(graph, i, visited, 1))
             if not result:
                 break
-    print("YES") if result else print("NO")
+    print('YES') if result else print('NO')
+
+
