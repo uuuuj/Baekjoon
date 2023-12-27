ea4e081d56e1b8030756f1619d24d4b91c660300
uuuuj <105343092+uuuuj@users.noreply.github.com>
2023-12-27 21:21:26 +0900

    [Bronze II] Title: 블랙잭, Time: 88 ms, Memory: 37492 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.py
+++ 백준/Bronze/2798. 블랙잭/블랙잭.py
@@ -1,13 +1,20 @@
 import sys
 
 N, M = map(int, sys.stdin.readline().split())
-card = list(map(int, sys.stdin.readline().split()))
-result = 0
-for i in range(N):
-    for j in range(i+1, N):
-        for k in range(j+1, N):
-            if card[i] + card[j] + card[k] > M:
-                continue
-            else:
-                result = max(result, card[i] + card[j] + card[k])
-print(result)
+card_list = list(map(int, sys.stdin.readline().split()))
+hap = 0
+hap_list = []
+for i in range(len(card_list)):
+    for j in range(i+1, len(card_list)):
+        for k in range(j+1, len(card_list)):
+            hap = card_list[i] + card_list[j] + card_list[k]
+            if hap <= M:
+                hap_list.append(hap)
+
+if M in hap_list:
+    print(M)
+else:
+    print(max(hap_list))
+
+
+
