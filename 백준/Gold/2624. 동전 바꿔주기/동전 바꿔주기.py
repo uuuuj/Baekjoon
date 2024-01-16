67abf808b673f4b08569a20540bcfc27c65af654
uuuuj <105343092+uuuuj@users.noreply.github.com>
2024-01-16 15:39:56 +0900

    [Gold V] Title: 동전 바꿔주기, Time: 220 ms, Memory: 110600 KB -BaekjoonHub

--- 백준/Gold/2624. 동전 바꿔주기/동전 바꿔주기.py
+++ 백준/Gold/2624. 동전 바꿔주기/동전 바꿔주기.py
@@ -1,16 +1,17 @@
 import sys
 
 T = int(sys.stdin.readline())
-k = int(sys.stdin.readline())
-coins = []
-for i in range(k):
-    a, b = map(int, sys.stdin.readline().split())
-    coins.append([a, b])
+K = int(sys.stdin.readline())
+cash = []
 dp = [0] * (T+1)
 dp[0] = 1
-for coin, cnt in coins:
+for _ in range(K):
+    coin, cnt = map(int, sys.stdin.readline().split())
+    cash.append([coin, cnt])
+
+for coin, count in cash:
     for money in range(T, 0, -1):
-        for i in range(1, cnt+1):
+        for i in range(1, count+1):
             if money - coin * i >= 0:
-                dp[money] += dp[money-coin*i]
+                dp[money] += dp[money - coin * i]
 print(dp[T])
