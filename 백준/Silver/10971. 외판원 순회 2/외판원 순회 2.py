1f80a9a333c2df53e3a684855b2cf8f448da8b48
uuuuj <105343092+uuuuj@users.noreply.github.com>
2024-02-12 20:59:23 +0900

    [Silver II] Title: 외판원 순회 2, Time: 56 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.py
+++ 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.py
@@ -1,29 +1,26 @@
 import sys
 
-N = int(sys.stdin.readline())
 
-cost_array = []
-
-for i in range(N):
-    array = list(map(int, sys.stdin.readline().split()))
-    cost_array.append(array)
-
-visited = [False] * N
-#초기값 업데이트를 저장하는 전역변수
-min_cost = 1e9
-def dfs(depth, start, cost):
-    #전역변수를 업데이트할때
-    global min_cost
-    if depth == N-1 and cost_array[start][0] != 0:
-        min_cost = min(min_cost, cost + cost_array[start][0])
+def dfs(start, now, value, cnt):
+    global ans
+    if cnt == N:
+        if W[now][start]:
+            value += W[now][start]
+            if ans > value:
+                ans = value
+        return
+    if value > ans:
         return
     for i in range(N):
-        if not visited[i] and cost_array[start][i] != 0:
-            visited[i] = True
-            dfs(depth+1, i, cost + cost_array[start][i])
-            visited[i] = False
-
-visited[0] = True
-
-dfs(0,0,0)
-print(min_cost)
+        if not visited[i] and W[now][i]:
+            visited[i] = 1
+            dfs(start, i, value + W[now][i], cnt + 1)
+            visited[i] = 0
+N = int(sys.stdin.readline())
+W = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
+ans = sys.maxsize
+visited = [0] * N
+for i in range(N):
+    visited[i] = 1
+    dfs(i, i, 0, 1)
+print(ans)
